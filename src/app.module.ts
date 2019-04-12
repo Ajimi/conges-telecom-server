8840fa0093e5a107372697b27415b9c94959b173
Selim <sloumma.21@gmail.com>
2019-04-12 15:26:57 +0100

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { Database } from './database/database';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService, Database],
+})
+export class AppModule {
+}
